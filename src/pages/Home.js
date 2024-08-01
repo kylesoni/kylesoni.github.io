0f3ee2087fcc0d82cd3cd8f427d41af28178d77a
kylesoni <kylesoni2142@gmail.com>
2024-08-01 00:05:02 -0700

    links and visuals improvement

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,19 +3,27 @@ import LinkedInIcon from "@material-ui/icons/LinkedIn"
 import EmailIcon from "@material-ui/icons/Email"
 import GitHubIcon from "@material-ui/icons/GitHub"
 import "../styles/Home.css"
+import Portrait from "../assets/KyleSoniPortrait.png"
 
 function Home() {
   return (
     <div className="home">
         <div className="about"> 
-            <h2> Hi, My Name is Kyle</h2>
+            <img src={Portrait} alt=""/>
+            <h2> Kyle Soni</h2>
             <div className="prompt"> 
                 <p> 
                     A software and data engineer. 
-                </p> 
-                <LinkedInIcon />
-                <EmailIcon />
-                <GitHubIcon />
+                </p>
+                <a href='https://www.linkedin.com/in/kyle-soni-581636170/'>
+                    <LinkedInIcon />
+                </a>
+                <a href='mailto:kylesoni2142@gmail.com'>
+                    <EmailIcon />
+                </a>
+                <a href='https://github.com/kylesoni'>
+                    <GitHubIcon />
+                </a>
             </div>
         </div>
         <div className="skills">
